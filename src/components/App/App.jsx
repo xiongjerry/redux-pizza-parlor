4a7450d64509649ed3419346dd4ecac5f054b8a7
xiongster94 <xiongster94@users.noreply.github.com>
2021-06-10 13:38:34 -0500

    made a navigation bar

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -16,7 +16,21 @@ function App() {
 
         <header className='App-header'>
           <h1 className='App-title'>Prime Pizza</h1>
+          <nav>
+            <ul className="App-header-ul">
+              <li>
+                <Link to="/">Home </Link>
+              </li>
+              <li>
+                <Link to="/step2">Customer Info</Link>
+              </li>
+              <li>
+                <Link to="/step3">Checkout</Link>
+              </li>
+            </ul>
+          </nav>
         </header>
+
 
         <Route path='/' exact>
           <Step1 />

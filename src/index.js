14e8818d9caf16eb84a1ec1dfd1e5e6b4d0166e4
Ryan Maas <rdmjobs@live.com>
2021-06-10 14:05:21 -0500

    Setup structure for step1.

--- src/index.js
+++ src/index.js
@@ -6,6 +6,15 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux'; 
 import logger from 'redux-logger';
 
+// Reducers: 
+const pizzaList = (state =[], action) => {
+  switch (action.type) {
+    case 'GET_PIZZA':
+      return state;
+    default: 
+      return state;
+  } // End switch
+} // End pizzaList reducer
 
 const store = createStore(
     combineReducers({

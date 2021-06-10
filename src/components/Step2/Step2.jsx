ad0bd0926a05872bf46caf20f5f5b8dd69c23937
Stephen Piper <stephen.r.piper@gmail.com>
2021-06-10 14:21:52 -0500

    good start on forms

--- src/components/Step2/Step2.jsx
+++ src/components/Step2/Step2.jsx
@@ -1,8 +1,57 @@
-function Step2(){
-    return (
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+
+
+function Step2() {
+  const dispatch = useDispatch();
+
+  const [name, setName] = useState('');
+  const [address, setAddress] = useState('');
+  const [city, setCity] = useState('');
+  const [zip, setZip] = useState('');
+  const [delivery, setDelivery] = useState('');
+
+
+  const handleSubmit = () => {
+    
+  }
+
+  return (
       <>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          required
+          placeholder="Name"
+          value={name}
+          onChange={(event) => setName(event.target.value)} />
+
+        <input
+          required
+          placeholder="Address"
+          value={address}
+          onChange={(event) => setAddress(event.target.value)} />
+
+        <input
+          required
+          placeholder="City"
+          value={city}
+          onChange={(event) => setCity(event.target.value)} />
+
+        <input
+          required
+          placeholder="Zip"
+          value={zip}
+          onChange={(event) => setZip(event.target.value)} />
+
+        <button type = "submit" >
+          Next
+        </button>
+        
+          </form>
+        
       </>
-    )
+    );
 }
 
 export default Step2;
